052f33fa463da726c70131b847d899f327a42c09
ralfgraef <graef.ralf@gmail.com>
2018-12-06 23:06:07 +0100

    create and index projects

--- server/start/routes.js
+++ server/start/routes.js
@@ -14,9 +14,13 @@
 */
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
-const Route = use("Route");
+const Route = use("Route")
 
 Route.group(() => {
-  Route.post("auth/register", "UserController.register");
-  Route.post("auth/login", "UserController.login");
-}).prefix("api");
+  Route.post('auth/register', 'UserController.register')
+  Route.post('auth/login', 'UserController.login')
+
+  Route.get('projects', 'ProjectController.index').middleware('auth')
+  Route.post('projects', 'ProjectController.create').middleware('auth')
+
+}).prefix("api")
