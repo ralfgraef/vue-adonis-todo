a3bd8d2119c2911adcc1fc18b8830c95aaf73ac0
ralfgraef <graef.ralf@gmail.com>
2018-12-08 21:11:55 +0100

    destroy + update task

--- server/app/Controllers/Http/TaskController.js
+++ server/app/Controllers/Http/TaskController.js
@@ -28,6 +28,29 @@ class TaskController {
     return task
   }
 
+  async destroy({ auth, request, params }) {
+    const user = await auth.getUser()
+    const id = params.id //const {id} = params
+    const task = await Task.find(id)
+    const project = await task.project().fetch
+    AuthorizationService.verifyPermission(project, user)
+    await task.delete()
+    return task
+  }
+
+  async update({ auth, request, params }) {
+    const user = await auth.getUser()
+    const id = params.id //const {id} = params
+    const task = await Task.find(id)
+    const project = await task.project().fetch
+    AuthorizationService.verifyPermission(project, user)
+    task.merge(request.only([
+      'description',
+      'completed',
+    ]))
+    await task.save()
+    return task
+  }
 }
 
 module.exports = TaskController
