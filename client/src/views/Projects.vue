bf45072327465db892de8198e7c110b7d7c50bf6
ralfgraef <graef.ralf@gmail.com>
2018-12-11 21:33:04 +0100

    add tasks panel

--- client/src/views/Projects.vue
+++ client/src/views/Projects.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="home">
-    <Panel title="projects"></Panel>
-  </div>
+  <v-container>
+    <v-layout>
+      <v-flex xs4>
+        <Panel title="Projects">
+          <h1>TESTING</h1>
+        </Panel>
+      </v-flex>
+      <v-flex xs8 class="pl-4">
+        <Panel title="Tasks">
+          <h1>TESTING</h1>
+        </Panel>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
